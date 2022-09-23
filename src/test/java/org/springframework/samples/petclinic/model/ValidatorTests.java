21f4f2908e4ab7b85d3ab55e6e41b9ebafeb4d33
Waer1 <elwaeryousef@gmail.com>
2022-09-23 11:54:47 +0200

    edit test to try using hook trigger

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -28,10 +28,6 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * @author Michael Isvy Simple test to make sure that Bean Validation is working (useful
- * when upgrading to a new version of Hibernate Validator/ Bean Validation)
- */
 class ValidatorTests {
 
 	private Validator createValidator() {
@@ -45,7 +41,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("empty");
+		person.setFirstName("emptty");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
